07ed89d1d09b2eb72bb4f33aca071807136c9683
MHML <mhml1367@gmail.com>
2020-06-13 20:05:31 +0430

    update

--- resources/views/hotel/booking.blade.php
+++ resources/views/hotel/booking.blade.php
@@ -17,7 +17,11 @@
                                 <tbody>
                                     <tr>
                                         <td class="bookex">نام و نام خانوادگی:</td>
-                                        <td>{{$rec->first_name}}  {{$rec->last_name}}</td>
+                                        <td>@if ($rec->Sir_Madam == "F")
+                                                خانم
+                                            @endif @if ($rec->Sir_Madam == "M")
+                                            آقا
+                                        @endif   {{$rec->first_name}}  {{$rec->last_name}}</td>
                                     </tr>
                                     <tr>
                                         <td class="bookex">کدملی:</td>
@@ -32,21 +36,13 @@
                                         <td>{{$rec->city}}</td>
                                     </tr>
                                     <tr>
-                                        <td class="bookex">آقا/خانم:</td>
-                                        <td>@if ($rec->Sir_Madam == "M")
-                                            خانم
-                                        @endif @if ($rec->Sir_Madam == "F")
-                                        آقا
-                                    @endif</td>
-                                    </tr>
-                                    <tr>
                                         <td class="bookex">اتاق:</td>
-                                        <td>{{$rec->room}}
+                                        <td>
                                         </td>
                                     </tr>
                                     <tr>
                                         <td class="bookex">اقامت:</td>
-                                        <td>{{$rec->contract}}
+                                        <td>
                                         </td>
                                     </tr>
                                     <tr>
@@ -72,17 +68,6 @@
                                     <button class="btn btn-primary btn-lg btn-grad" id="send" type="submit">تایید رزرو و پرداخت</button>
                                 </div>
                             </div>
-
-
-
-                            <div class="login-top cardInfo">
-                                <h3>مشاهده جزئیات رزرو</h3>
-                                <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
-                                    گرافیک است.
-                                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان
-                                    گرافیک است.
-                                </p>
-                            </div>
                         </div>
 
                     </div>
@@ -92,63 +77,31 @@
             <div class="col-lg-4 col-md-6 col-sm-12">
                 <div class="listing-item ">
                     <article class="TravelGo-category-listing fl-wrap">
-                        <div class="TravelGo-category-img"> <a href="hotel-detailed.html"><img
-                                    src="images/hotels/room8.jpg" alt=""></a>
-                            <div class="TravelGo-category-opt">
-                                <div class="listing-rating card-popup-rainingvis" data-starrating2="5"><i
-                                        class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i
-                                        class="fa fa-star"></i><i class="fa fa-star"></i></div>
-                                <div class="rate-class-name">
-                                    <div class="score"><strong>بسیار خوب</strong> 27 بررسی</div>
-                                    <span>5.0</span>
-                                </div>
-                            </div>
-                        </div>
                         <div class="TravelGo-category-content fl-wrap title-sin_item">
                             <div class="TravelGo-category-content-title fl-wrap">
                                 <div class="TravelGo-category-content-title-item">
-                                    <h3 class="title-sin_map"><a href="hotel-detailed.html">کروز آسیا و آفریقا</a></h3>
+                                    <h3 class="title-sin_map"><a href="hotel-detailed.html">{{$rec->room}}</a></h3>
                                     <div class="TravelGo-category-location fl-wrap"><a href="#" class="map-item"><i
-                                                class="fas fa-map-marker-alt"></i>بیست و هفتم بروکلین نیویورک ، </a>
-                                        <span>200 تومان </span><a href="#" class="map-item"></a><span></span> </div>
+                                                class="fas fa-map-marker-alt"></i>{{$rec->hotel}}</a>
+                                        <span>{{$rec->price}}</span><a href="#" class="map-item"></a><span></span> </div>
                                 </div>
                             </div>
-                            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک
-                                است.</p>
-                            <ul class="facilities-list fl-wrap">
-                                <li><i class="fas fa-wifi"></i><span>وای فای رایگان</span></li>
-                                <li><i class="fas fa-parking"></i><span>پارکینگ</span></li>
-                                <li><i class="fas fa-smoking-ban"></i><span>اتاقهای غیر سیگاری</span></li>
-                                <li><i class="fas fa-utensils"></i><span> رستوران</span></li>
-                            </ul>
-                            <div class="TravelGo-category-footer fl-wrap">
-                                <div class="TravelGo-category-price btn-grad"><span>ویرایش کنید</span></div>
-                            </div>
+                            
+                            
                             <div class="TravelGo-category-content-title-item others-details">
-                                <h3 class="title-sin_map"><a href="hotel-detailed.html">جزئیات دیگر</a></h3>
+                                <h3 class="title-sin_map"><a href="hotel-detailed.html">جزئیات رزرو</a></h3>
                             </div>
                             <table class="table table-striped">
                                 <tbody>
                                     <tr>
                                         <td class="bookex">نوع اتاق:</td>
-                                        <td>لوکس</td>
+                                        <td>{{$rec->room}}</td>
                                     </tr>
                                     <tr>
-                                        <td class="bookex">غذا و ناهار خوری:</td>
-                                        <td>200 تومان</td>
+                                        <td class="bookex">اقامت:</td>
+                                        <td>{{$rec->contract}}</td>
                                     </tr>
-                                    <tr>
-                                        <td class="bookex">CRUISE PRICE:</td>
-                                        <td>300 تومان</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="bookex">مالیات:</td>
-                                        <td>300 تومان</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="bookex"><strong>جمع:</strong></td>
-                                        <td><strong>1200 تومان</strong></td>
-                                    </tr>
+                                
                                 </tbody>
                             </table>
                         </div>
